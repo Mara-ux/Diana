d3538733073b64617719dec6e14a2ff0804908f0
Mara <chonvon2021@gmail.com>
2025-01-20 20:35:49 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,20 @@
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
+        `
+    })
+})
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
